45c55a5035f75ce3187d49be7cfccc77edb0ac6f
Fabio Silveira <fabio.eds@hotmail.com>
2025-05-08 09:15:57 -0700

    AddedLogicforBtnsIncrease

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { useEffect, useState } from "react"
 export default function App() {
 
   const [data, setData] = useState(null);
+  const [counter, setCounter] = useState(1);
 
   useEffect(() => {
     async function fetchApi() {
       const req = await axios.get("https://681504bd225ff1af162adcf7.mockapi.io/shoes/1")
       setData(req.data)
+      setCounter(req.data.amount)
     }
 
     fetchApi()
@@ -27,9 +29,19 @@ export default function App() {
           </div>
 
           <div className="div-btns-amount">
-            <button className="btns-amount">+</button>
-            <h3 className="h3-amount">{data.amount}</h3>
-            <button className="btns-amount">-</button>
+            <button onClick={() => counter < data.max_amount && setCounter(counter + 1)}
+              className="btns-amount"
+              disabled={counter >= data.max_amount}>
+              +
+            </button>
+
+            <h3 className="h3-amount">{counter}</h3>
+
+            <button onClick={() => counter > data.amount && setCounter(counter - 1)}
+              className="btns-amount"
+              disabled={counter <= data.amount}>
+              -
+            </button>
           </div>
 
           <h2 className="h2-price">TOTAL VALUE:</h2>
