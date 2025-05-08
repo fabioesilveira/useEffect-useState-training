6f2c01c123e5276c1127421ba436cbc73eb239e8
Fabio Silveira <fabio.eds@hotmail.com>
2025-05-07 22:49:24 -0700

    AddedStyle

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,28 @@ export default function App() {
 
   return (
     <div className="my-container">
+
       {!data ? <span>Loading</span> : (
         <div>
-          <h1>Shopping Cart</h1>
-          <img className="img-home" src={data.thumb} />
+          <h1 className="h1-home">SHOPPING CART</h1>
 
-          <button>+</button>
-          <h3>{data.amount}</h3>
-          <button>-</button>
-          <h2>TOTAL VALUE</h2>
-          <h2>${data.price}</h2>
-          <button>CHECKOUT</button>
+          <div className="div-img">
+
+            <img className="img-home" src={data.thumb} />
+          </div>
+
+          <div className="div-btns-amount">
+            <button className="btns-amount">+</button>
+            <h3 className="h3-amount">{data.amount}</h3>
+            <button className="btns-amount">-</button>
+          </div>
+
+          <h2 className="h2-price">TOTAL VALUE:</h2>
+          <h2 className="price">${data.price}</h2>
+
+          <div className="div-checkout">
+            <button className="btn-checkout">CHECKOUT</button>
+          </div>
         </div>
       )}
     </div>
